13036d26655949e7d4ad641f5cc0ed2f679ada41
Jatded1d9l7 <jthoma25wv@gmail.com>
2021-07-12 13:24:22 -0400

    review for coding concepts answers

--- coding-concepts.js
+++ coding-concepts.js
@@ -16,6 +16,9 @@
 // 12 place holder positions counting from "C"=0 through "1"= 12. I answered this question wrong
 //because the zero index does not apply to .length
 
+
+// Review: Answer-- the number of characters in the string
+
 // --------------------2) What will this log?
 
 // var greeting = "Hello World!"
@@ -26,6 +29,9 @@
 // b) Verify and explain: The answer was indeed l. This question confirmed my query in the above qestion
 //zero indexing does not apply to .length, yet rather it does apply to direct bracket[] indexing
 
+
+// Review: Answer: "l" 3 is an index so we are pulling a value from the string based on the index.
+// we use the [] as the index
 
 // --------------------3) What will this log?
 
@@ -43,6 +49,11 @@
 //
 //b) Verify and explain: My answer was incorrect. The correct answer is Ruby. Perhaps
 // this is because the array is also zero indexed, begining with JavaScript at position 0
+
+// Review: Answer- ruby because it is at the first indexed
+// using the same index but structuring it differently
+
+
 
 
 // --------------------4) What will this log?
@@ -62,10 +73,33 @@
 // rendered "weekendDays.toUpperCase is not a function"
 
 
+// Review: Answer- the  built in method only works with a string, and not an array.
+//- weekend days is an array
+//- .toUpperCase will not work with an array
+//- this will render an error message.
+//- this is a fundemental error that is seen all the time
+//- this function can not be applied to this data type
+
+
 // --------------------5) What will this log?
 
 // var dataTypes = ["number", "string", "Boolean"]
 // console.log(typeof dataTypes.length)
+
+
+// Review- Answer the answer is number.
+//- Specifically, we get the string containing the word number
+//- the first thing that happens is .length
+//- .length would give us =3
+//--then calling type of on 3
+//- typeof is an interesting operator
+//- typeof doesnt play by the rules because it is an old operator
+//- typeof is fundamental to to the language
+//- doesnt have parentheses, it stands alone and takes an argument
+//- typeof returns a string containing the lable of that data type
+
+//-* .length is always going to be a number
+
 
 // a) Your answer: number because the typeof function is describing the length of the datatype array
 // using zero indexing makes "number"= 0, "sting"= 1, and "Boolean"=2
