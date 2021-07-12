178f720f10514bd8a84808b99706e601331620eb
Jatded1d9l7 <jthoma25wv@gmail.com>
2021-07-12 10:41:49 -0400

    updated challenges- started on interview challenges

--- code-challenges.js
+++ code-challenges.js
@@ -17,28 +17,43 @@
 // --------------------1) Create a function that takes in a number and determines if a temperature is below boiling point, at boiling point or above boiling point. Boiling point is 212 degrees Fahrenheit.
 // Use the test variables provided below. Expected output: "35 is below boiling point", "350 is above boiling point", "212 is at boiling point"
 
+//EXAMPLE//
+// // create a function called tallEnough that takes in a number called height
+// const tallEnough = (height) => {
+//   if(height < 40) {
+//   }
+//   else if(height >= 40) {
+//     return "Buckle up, let's ride"
+//   }
+//   // any other input return error
+//   else {
+//     return "error"
+//   }
+// }
+// EXAMPLE
+
+
+
+//
 // var temp1 = 35
 // var temp2 = 350
 // var temp3 = 212
 //
-// const boilingPoint = (temp1, temp2, temp3) => {
-//   if (temp1 > 212)}
-//   return `${temp1} is above boiling point`
-// } else if (temp1 === 212){
-//   return `${temp1} is at boiling point`
-// } else if (temp1 < 212){
-//   return `${temp1} is below boiling point`
-// }
-// }
-//
-// console.log boilingPoint()
-//
+const boilingPoint = (boil) => {
+  if (boil > 212) {
+  }
+  return `${boil} is above boiling point `
+} else if (boil < 212) {
+  return `${boil} is below boiling point `
+  }
+
+console.log(boilingPoint(213))
 
 // --------------------2) Create a function that takes in two arrays of numbers and returns a single array with all the values sorted least to greatest.
 // Use the test variables provided below. Expected output: [-9, -7, 0, 3, 7, 8, 9, 13, 22, 36]
 
 // var myNumbers1 = [3, 7, 0, 36, -9]
-// var myNumbers2 = [8, -7, 22, 9, 13]
+// var myNumbers2 = [ , 8, -7, 22, 9, 13]
 //
 // var singleSortedArray = (myNumbers1 + myNumbers2)
 //
@@ -47,28 +62,39 @@
 // --------------------3) Create a function that takes in a string of a single word and returns the string with all letters reversed.
 // Use the test variables provided below. Expected output: "ovarb", "eilrahc"
 
-var myString1 = "bravo"
-var myString2 = "charlie"
-
-
+// var myString1 = "bravo"
+// var myString2 = " charlie"
+//
+// const singleWord = (myString1 + myString2)
+//
+// console.log(singleWord)
 
 // --------------------4) Create a function that takes in two numbers and subtracts the smaller number from the larger number.
 // Use the two sets of test variables provided below. Expected output: 15, 12
 
-var numberExample1 = 42
-var numberExample2 = 27
+// var numberExample1 = 42
+// var numberExample2 = 27
+//
+// var numberExample3 = 7
+// var numberExample4 = 19
+//
+// const subTwo = (numberExample1, numberExample2) => {
+//   if numberExample1 > numberExample2
+//   return numberExample1 - numberExample2
+// }
+// }
+// console.log(subTwo)
 
-var numberExample3 = 7
-var numberExample4 = 19
 
+// const subTwo = (numberExample1 + numberExample2 + numberExample3 + numberExample4)
 
 
 // --------------------5) Copy/paste your code from #4. Refactor your code to also accept non-number edge cases. Inform your user if the variable passed into the subtraction function is not a number.
 // Use the two sets of test variables provided below. Expected output: "Your input is not a number", 3
 // Hint: There is an operator in JavaScript called typeof :)
 
-var numberExampleRefactor1 = 42
-var numberExampleRefactor2 = "hello"
-
-var numberExampleRefactor3 = 27
-var numberExampleRefactor4 = 24
+// var numberExampleRefactor1 = 42
+// var numberExampleRefactor2 = "hello"
+//
+// var numberExampleRefactor3 = 27
+// var numberExampleRefactor4 = 24
--- interview-questions.md
+++ interview-questions.md
@@ -7,7 +7,7 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is a function? Why would you use one?
 
-  Your answer:
+  Your answer: A function is an operation that you would like to run. You would use a function to express logic.
 
   Researched answer:
 
@@ -15,7 +15,7 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 2. In regards to functions, what is the difference between an argument and a parameter?
 
-  Your answer:
+  Your answer: An argument is a single point of logic while a parameter is the entire logic.
 
   Researched answer:
 
@@ -23,7 +23,7 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 3. What is an index? What is the difference between index and value?
 
-  Your answer:
+  Your answer: An index is a given location for a data point.
 
   Researched answer:
 
@@ -31,7 +31,7 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 4. What is a built-in method?
 
-  Your answer:
+  Your answer: Built-in methods are functions that are built into the programming language and are used for common functions.
 
   Researched answer:
 
@@ -39,7 +39,7 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 5. Give a brief description of proper pair programming techniques. What are the roles of each person?
 
-  Your answer:
+  Your answer: There is one driver and one person actually doing the coding. While pair programing there is only one working file being worked on. The navigators job is to direct the coder. While the coder is responsible for writing the code.
 
   Researched answer:
 
@@ -47,7 +47,7 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 6. What is something we did in class this week you found helpful?  
 
-  Your answer:
+  Your answer: This week I found learning the terminal, terminal commands, git, and github helpful. Everything is so new to me, so learning all of this was mind blowing for me. 
 
 
 
